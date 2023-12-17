139288ee9d7cdea8c97751f4f5eacb962e8280b6
Pingying Chen <30317248+Pingying-Chen@users.noreply.github.com>
2023-12-17 13:44:56 -0800

    Create 2890.py

---
+++ 2890.py
@@ -0,0 +1,11 @@
+# 2890. Reshape Data: Melt
+
+import pandas as pd
+
+def meltTable(report: pd.DataFrame) -> pd.DataFrame:
+    return pd.melt(
+        report,
+        id_vars = ['product'],
+        var_name = 'quarter',
+        value_name = 'sales'
+        )
