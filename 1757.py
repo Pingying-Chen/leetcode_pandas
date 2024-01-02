8381b052a4b392f289ca62ce0f4cc273140ee07f
Pingying Chen <30317248+Pingying-Chen@users.noreply.github.com>
2024-01-01 18:32:44 -0800

    Update 1757.py

--- 1757.py
+++ 1757.py
@@ -6,6 +6,8 @@ def find_products(products: pd.DataFrame) -> pd.DataFrame:
     df = products[(products['low_fats'] == 'Y') & (products['recyclable'] == 'Y')]
     return df[['product_id']]
 
+def find_products(products: pd.DataFrame) -> pd.DataFrame:
+    return products[(products['low_fats'] == 'Y') & (products['recyclable'] == 'Y')][['product_id']]
 
 def find_products(products: pd.DataFrame) -> pd.DataFrame:
     return products.loc[(products['low_fats'] == 'Y') & (products['recyclable'] == 'Y'), ['product_id']]
