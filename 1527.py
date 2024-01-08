b4e551d875f6f2223056df823055f27f8ec20a53
Pingying Chen <30317248+Pingying-Chen@users.noreply.github.com>
2024-01-07 23:08:47 -0800

    Create 1527.py

---
+++ 1527.py
@@ -0,0 +1,6 @@
+# 1527. Patients With a Condition
+
+import pandas as pd
+
+def find_patients(patients: pd.DataFrame) -> pd.DataFrame:
+    return patients[patients['conditions'].str.contains(r'\bDIAB1')]
