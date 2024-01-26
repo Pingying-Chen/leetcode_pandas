e321e2344ea258127a805b3ef50e18f4ac79475e
Pingying Chen <30317248+Pingying-Chen@users.noreply.github.com>
2024-01-25 22:02:54 -0800

    Create 175.py

---
+++ 175.py
@@ -0,0 +1,7 @@
+# 175. Combine Two Tables
+
+import pandas as pd
+
+def combine_two_tables(person: pd.DataFrame, address: pd.DataFrame) -> pd.DataFrame:
+    df = person.merge(address, on = 'personId', how = 'left')
+    return df[['firstName', 'lastName', 'city', 'state']]
